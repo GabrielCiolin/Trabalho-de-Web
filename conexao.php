ec7b164d4f69e4173f84b5949a75a24932562642
Vitor.Gibim <vitor.gibim@edu.unipar.br>
2022-11-19 17:17:49 -0300

    ADD Conection

---
+++ conexao.php
@@ -0,0 +1,29 @@
+<?php
+
+session_start();
+
+$localhost = "localhost";
+$user = "";
+$password = "";
+$db = "sistema";
+
+
+global $pdo;
+
+try {
+
+    //orientada a objetos com pdo
+    $pdo = new PDO("mysql:dbname=".$db."; host=".$localhost, $user, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+} catch (PDOException $e) {
+    echo "ERRO: " .$e->getMessage();
+    exit;
+}
+
+
+
+
+
+
+?>
